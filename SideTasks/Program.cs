15a5e7330d23ad563fc0def17d95ee108a4275d3
makosteszta007 <moleve2000@stud.uni-obuda.hu>
2023-11-24 21:13:02 +0100

    first side task done

---
+++ SideTasks/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace SideTasks
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double N = double.Parse(Console.ReadLine());
+            double result = -2;
+            char[] NArray = N.ToString().ToCharArray();
+            while (result==-2)
+            {
+                N++;               
+                double counter = 0;
+                NArray = N.ToString().ToCharArray();
+                if (NArray.Length>1)
+                {
+                    for (int i = 0; i < NArray.Length - 1; i++)
+                    {
+                        if (NArray[i + 1] >= NArray[i])
+                        {
+                            counter++;
+                        }
+                    }
+                    if (counter == NArray.Length-1) { result = double.Parse(NArray); }                   
+                }
+                else
+                {
+                    result = double.Parse(NArray);
+                }
+            }
+            Console.WriteLine(result);
+        }
+    }
+}
